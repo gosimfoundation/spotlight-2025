657692c90cc7cafa12ca210e99d434b989a8efa0
AIFC2025 <yingtongzhou123@gmail.com>
2025-07-14 06:21:35 +0800

    Fix: set correct site and base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
-  site: "https://testnew.gosim.org",
+  site: 'https://gosimfoundation.github.io',
+  base: '/spotlight-2025/',
 });
